d1a69d34a16e6663bf38564356395fe19ad6a8d6
mattiassvedhem <mattias@kyparn.se>
2012-11-20 15:00:56 +0100

    Add failing test for draper decorators.

---
+++ test/serialization_draper_decorator_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+require 'draper'
+
+class Some
+	include ActiveModel::Serialization
+
+	def title
+		"Some stored title"
+	end
+end
+
+class SomeDecorator < Draper::Base
+	decorates :some
+
+	def title
+		"Some processed title"
+	end
+end
+
+class SomeSerializer < ActiveModel::Serializer
+	attributes :title
+end
+
+class SerializationDraperDecoratorTest < ActionController::TestCase
+  def test_uses_decorated_objects_methods
+    some_serialized = SomeSerializer.new(SomeDecorator.new(Some.new))
+    assert_equal SomeDecorator.new(Some.new).title, some_serialized.title
+  end
+end
