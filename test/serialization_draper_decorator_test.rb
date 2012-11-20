a8bd60fd146dc96fefaa5fb9386135b1f2630691
mattiassvedhem <mattias@kyparn.se>
2012-11-20 16:11:37 +0100

    Improve draper test to show that decorated object is never used.

--- test/serialization_draper_decorator_test.rb
+++ test/serialization_draper_decorator_test.rb
@@ -15,15 +15,24 @@ class SomeDecorator < Draper::Base
   def title
     "Some processed title"
   end
+
+  def name
+    "Something"
+  end
 end
 
 class SomeSerializer < ActiveModel::Serializer
-  attributes :title
+  attributes :title, :name
 end
 
 class SerializationDraperDecoratorTest < ActiveModel::TestCase
-  def test_uses_decorated_objects_methods
+  def test_uses_overriden_methods
     some_serialized = SomeSerializer.new(SomeDecorator.new(Some.new))
     assert_equal SomeDecorator.new(Some.new).title, some_serialized.title
+  end
+
+  def test_uses_decorator_methods
+    some_serialized = SomeSerializer.new(SomeDecorator.new(Some.new))
+    assert_equal SomeDecorator.new(Some.new).name, some_serialized.name
   end
 end
